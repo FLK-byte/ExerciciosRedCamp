691100be5c9fbb71dd6b029f6eac40b8e0bfd32d
Filipe <llyphy46@gmail.com>
2022-04-20 15:32:25 -0300

    Refact

--- exercicios/terceira_semana/diaUm/src/controller/user.js
+++ exercicios/terceira_semana/diaUm/src/controller/user.js
@@ -5,7 +5,6 @@ exports.getUsers = async (req, res) => {
     const { data, status } = await getAllUsers()
     res.status(status).json(data)
 }
-
 exports.createUser = async (req, res) => {
     const { name, email } = req.body
     if (name && email) {
@@ -15,14 +14,12 @@ exports.createUser = async (req, res) => {
                 const { data, status } = await createOneUser(req.body)
                 return res.status(status).json(data)
             }
-            return res.status(404).json({ messageError: "O email já está sendo utilizado" })
-        } else {
-            return res.status(404).json({ messageError: "O seu email é inválido" })
+            return res.status(400).json({ messageError: "O email já está sendo utilizado" })
         }
+        return res.status(400).json({ messageError: "O seu email é inválido" })
     }
-    res.status(404).json({ messageError: "Preencha os campos corretamente" })
+    res.status(400).json({ messageError: "Preencha os campos corretamente" })
 }
-
 exports.getUser = async (req, res) => {
     try {
         const { data, status } = await getOneUserById(req.params.id)
@@ -31,48 +28,34 @@ exports.getUser = async (req, res) => {
         res.status(404).json({ messageError: `Não foi possivel encontrar o seguinte id: ${req.params.id}` })
     }
 }
-
 exports.put = async (req, res) => {
-    const userExistente = await getOneUserById(req.params.id)
-    if (userExistente.data) {
-        const { name, email } = req.body
-        if (name && email) {
-            if (validatorEmail(email)) {
-                const emailExistente = await getOneUserByEmail(email)
-                if (emailExistente.data == null) {
-                    const { data, status } = await putUser(req.params.id, req.body)
-                    return res.status(status).json(data)
-                }
-                return res.status(404).json({ messageError: "O email já está sendo utilizado" })
+    const { name, email } = req.body
+    try {
+        const userExistente = await getOneUserById(req.params.id)
+        if (userExistente !== null) {
+            if (!name.trim() || !email.trim()) {
+                return res.status(400).json({ messageError: "Preencha os campos corretamente" })
             }
-            return res.status(404).json({ messageError: "O seu email é inválido" })
-        } else if (name) {
-            req.body.email = userExistente.data.email
-            const { data, status } = await putUser(req.params.id, req.body)
-            return res.status(status).json(data)
-        } else if (email) {
-            if (validatorEmail(email)) {
-                const emailExistente = await getOneUserByEmail(email)
-                if (emailExistente.data == null) {
-                    req.body.name = userExistente.data.name
-                    const { data, status } = await putUser(req.params.id, req.body)
-                    return res.status(status).json(data)
-                }
-                return res.status(404).json({ messageError: "O email já está sendo utilizado" })
+            if (email && !validatorEmail(email)) {
+                return res.status(400).json({ messageError: "Email inválido" })
             }
-            return res.status(404).json({ messageError: "O seu email é inválido" })
+            let emailExistente = await getOneUserByEmail(email)
+            if (emailExistente.data == null) {
+                const { data, status } = await putUser(req.params.id, req.body)
+                return res.status(status).json(data)
+            }
+            return res.status(400).json({ messageError: "Email já cadastrado" })
         }
-        return res.status(404).json({ messageError: "Preencha os campos corretamente" })
+    } catch (err) {
+        return res.status(404).json({ messageError: "Usuario não existente" })
     }
-    res.status(404).json({ messageError: "Usuario Nao encontrado" })
 }
-
 exports.removeUser = async (req, res) => {
     try {
         const { data, status } = await removeUser(req.params.id)
         res.status(status).json(data)
     } catch (err) {
-        res.status(404).json({ messageError: "Usuario Nao encontrado verifique o ID inserido" })
+        res.status(404).json({ messageError: "Usuario não encontrado verifique o ID inserido" })
     }
 
 }
